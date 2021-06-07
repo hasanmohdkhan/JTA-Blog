8108e1b7d6971e4804bbb8ed313f7dfd19fce29e
Hasan Mhod Khan <hasanzian@gmail.com>
2021-06-07 10:18:41 +0530

    Delete BlogsService directory

--- BlogsService/src/main/java/com/jta/in/blog/constants/ApplicationConstants.java
+++
@@ -1,7 +0,0 @@
-package com.jta.in.blog.constants;
-
-public class ApplicationConstants {
-    private ApplicationConstants() {}
-    //Error messages
-    public static final String INVALID_CREDENTIALS = "Invalid credentials";
-}
