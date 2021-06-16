533fc99c3afd05d3d979eba10071732833509620
hasanmohdkhan <hasanzian@gmail.com>
2021-06-16 13:24:12 +0530

    dynamic discovery server setup

--- BlogServices/src/main/java/com/jta/in/blog/BlogController.java
+++ BlogServices/src/main/java/com/jta/in/blog/BlogController.java
@@ -1,9 +1,6 @@
 package com.jta.in.blog;
 
-import org.apache.ibatis.session.SqlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/blog")
 public class BlogController {
-    Logger logger = LoggerFactory.getLogger(BlogController.class);
-
 
     @GetMapping(value = "/home", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getSqlSession() {
-
+        System.out.println("----- Get request ----");
         return "<----> Hello from client blog <---->";
-
 
     }
 }
